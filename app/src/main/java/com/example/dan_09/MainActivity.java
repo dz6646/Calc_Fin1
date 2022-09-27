9bd38a9858f82ee131d29b177db091e51d24b80d
dz6646 <dz6646@bs.amalnet.k12.il>
2022-09-27 19:26:43 +0300

    Final commit

--- app/src/main/java/com/example/dan_09/MainActivity.java
+++ app/src/main/java/com/example/dan_09/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.dan_09;
+package com.example.dan_09; // Calculator 11th class :D
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -34,172 +34,178 @@ public class MainActivity extends AppCompatActivity {
         si = new Intent(this, Credits.class); // Source Intent
     }
 
+    /* *****************************************
+    This is the function for the addition method
+    ********************************************* */
     public void Plus(View view) {
         String st = "";
-        st = input.getText().toString();
+        st = input.getText().toString(); // Getting the input
+        if(!st.isEmpty()) // Input check 1
+        {
+            if (isNumeric(st))  // Input check 2
+            {
+                num1 = Float.parseFloat(st);
+                if (peoola == 0) // Calculations
+                {
+                    sum = num1;
+                }
+                else
+                {
+                    peoolot(peoola);
+                }
 
-        if(first == 1)
-        {
-            first = 0;
+            }
+            else {
+                Toast.makeText(this, "Error with the input", Toast.LENGTH_SHORT).show();
+            }
         }
-        if(!isNumeric(st)) // Input check
-        {
-            sum = 0;
-            num1 = 0;
-            first = 1;
-            input.setText("");
-            input.setHint("");
-        }
-        else if(!st.equals("")) // If it's empty don't do anything
-        {
-            num1 = Float.parseFloat(st);
-            sum += num1;
-        }
-
         input.setText("");
-        input.setHint(sum + " + "); // Show the final output
-        peoola = 1;
+        input.setHint(sum + " +"); // Update the screen
+        peoola = 1; // Update the calculation flag
     }
 
+    /* *****************************************
+    This is the function for the removal method
+    ********************************************* */
     public void minus(View view) {
         String st = "";
         st = input.getText().toString();
-
-        if(!isNumeric(st))
+        if(!st.isEmpty())
         {
-            sum = 0;
-            num1 = 0;
-            first = 1;
-            input.setText("");
-            input.setHint("");
-            Toast.makeText(this, "Wrong input!", Toast.LENGTH_LONG).show();
+            if (isNumeric(st))
+            {
+                num1 = Float.parseFloat(st);
+                if (peoola == 0)
+                {
+                    sum = num1;
+                }
+                else
+                {
+                    peoolot(peoola);
+                }
+            } else {
+                Toast.makeText(this, "Error with the input", Toast.LENGTH_SHORT).show();
+            }
         }
-        if(first == 1)
-        {
-            num1 = Float.parseFloat(st);
-            sum = num1;
-            first = 0;
-        }
-        else if(!st.equals("")) {
-            num1 = Float.parseFloat(st);
-            sum -= num1;
-        }
-
         input.setText("");
-        input.setHint(sum + " - ");
+        input.setHint(sum + " -");
         peoola = 2;
     }
 
+    /* *****************************************
+    This is the function for the multiplation method
+    ********************************************* */
     public void multiple(View view) {
-        if(first == 1 && sum == 0)
-        {
-            sum = 1;
-            first = 0;
-        }
-
         String st = "";
         st = input.getText().toString();
-
-        if(!isNumeric(st))
+        if(!st.isEmpty())
         {
-            sum = 0;
-            num1 = 0;
-            first = 1;
-            input.setText("");
-            input.setHint("");
-            Toast.makeText(this, "Wrong input!", Toast.LENGTH_LONG).show();
-        }
-        else if(!st.equals("")) {
-            num1 = Float.parseFloat(st);
-            sum *= num1;
-        }
+            if (isNumeric(st))
+            {
+                num1 = Float.parseFloat(st);
+                if (peoola == 0)
+                {
+                    sum = num1;
+                }
+                else
+                {
+                    peoolot(peoola);
+                }
 
+            }
+            else
+            {
+                Toast.makeText(this, "Error with the input", Toast.LENGTH_SHORT).show();
+            }
+        }
         input.setText("");
-        input.setHint(sum + " * ");
+        input.setHint(sum + " *");
         peoola = 3;
     }
 
+    /* *****************************************
+    This is the function for the divide method
+    ********************************************* */
     public void divide(View view) {
         String st = "";
         st = input.getText().toString();
-
-        if(!isNumeric(st))
+        if(!st.isEmpty())
         {
-            sum = 0;
-            num1 = 0;
-            first = 1;
-            input.setText("");
-            input.setHint("");
-            Toast.makeText(this, "Wrong input!", Toast.LENGTH_LONG).show();
-        }
-        else if(!st.equals("")) {
-            num1 = Float.parseFloat(st);
-            if (num1 == 0 || sum == 0) {
-                sum = 0;
+            if (isNumeric(st))
+            {
+                num1 = Float.parseFloat(st);
+                if (peoola == 0)
+                {
+                    sum = num1;
+                }
+                else
+                {
+                    peoolot(peoola);
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "Error with the input", Toast.LENGTH_SHORT).show();
             }
         }
-
         input.setText("");
-        input.setHint(sum + " / ");
+        input.setHint(sum + " /");
         peoola = 4;
     }
 
+    /* **************************************************************************
+    This function will check everything and calculate the final sum then show it
+    ***************************************************************************** */
     public void equals(View view) {
         String st = "";
         st = input.getText().toString();
-
-        if(!isNumeric(st))
+        if(!st.isEmpty())
         {
-            sum = 0;
-            num1 = 0;
-            first = 1;
-            input.setText("");
-            input.setHint("");
-            Toast.makeText(this, "Wrong input!", Toast.LENGTH_LONG).show();
+            if (isNumeric(st))
+            {
+                num1 = Float.parseFloat(st);
+                if (peoola == 0)
+                {
+                    sum = num1;
+                }
+                else
+                {
+                    peoolot(peoola);
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "Error with the input", Toast.LENGTH_SHORT).show();
+            }
         }
-        else if(!st.equals(""))
-        {
-            num1 = Float.parseFloat(st);
-        }
-        if(peoola == 1)
-        {
-            sum += num1;
-        }
-        else if(peoola == 2)
-        {
-            sum -= num1;
-        }
-        else if(peoola == 3)
-        {
-            sum *= num1;
-        }
-        else if(peoola == 4)
-        {
-            sum /= num1;
-        }
-        else
-        {
-            input.setHint("Error");
-        }
-
         input.setText("");
-        input.setHint("" + sum);
+        input.setHint(sum + "");
+        peoola = 0;
     }
 
+    /* ****************************
+    clear numbers and screen method
+    ******************************* */
     public void clear(View view) {
         sum = 0;
         num1 = 0;
         first = 1;
         input.setText("");
-        input.setHint("");
+        input.setHint("0");
         Toast.makeText(this, "Cleared successfully", Toast.LENGTH_LONG).show();
     }
 
+    /* ******************************
+    The Intent activity pass function
+    ********************************** */
     public void pass2Creds(View view) {
         si.putExtra("sum", sum);
         startActivityForResult(si, 8);
     }
 
+    /* ********************
+    Input checking function
+    ************************ */
     public static boolean isNumeric(String st)
     {
         int i = 0;
@@ -217,4 +223,30 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    /* **************************************************************************************
+    This function will check what method needs to be done on the sum and do it (Calculations)
+    ***************************************************************************************** */
+    public void peoolot(int peula)
+    {
+        switch(peula)
+        {
+            case 1:
+                sum += num1;
+                break;
+            case 2:
+                sum -= num1;
+                break;
+            case 3:
+                sum *= num1;
+                break;
+            case 4:
+                sum /= num1;
+                break;
+            default:
+                sum = num1;
+                break;
+        }
+    }
 }
+
